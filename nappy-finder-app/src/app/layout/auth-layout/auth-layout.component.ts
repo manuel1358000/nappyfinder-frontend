dde44d4ad00cac021dbf17811463d4289adc2c06
Manuel Fuentes <manuel.fuentes@curantissolutions.com>
2024-09-25 15:28:15 -0600

    update documents

--- nappy-finder-app/src/app/layout/auth-layout/auth-layout.component.ts
+++ nappy-finder-app/src/app/layout/auth-layout/auth-layout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from '../../material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaterialModule, CommonModule],
   templateUrl: './auth-layout.component.html',
-  styleUrl: './auth-layout.component.scss'
 })
 export class AuthLayoutComponent {
 
